959dbfc3c966efb0e7757dd0771380f31aad42ea
Miheer Vaidya <m.vaidya@utah.edu>
2021-01-02 17:17:56 -0700

    Remove Box around directives

--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,22 @@ mod tests {
     // tests is seperate module and needs import
     use super::maestro::{DirectiveParser, NumParser, VaridParser};
     use maestro_ast::Directive;
-    use std::borrow::Borrow;
-    use std::convert::AsRef;
     use std::str::FromStr;
     #[test]
     fn it_works() {
         assert!(NumParser::new().parse("123").unwrap() == 123u32);
         assert!(VaridParser::new().parse("x").unwrap() == String::from_str("x").unwrap());
         // assert!(DirectiveParser::new().parse("hi").is_ok());
-        match DirectiveParser::new()
-            .parse("SpatialMap (3,3)x;")
-            .unwrap()
-            .as_ref()
-        {
-            Directive::SpacialMap {
-                dimention,
-                size,
-                offset,
-            } => assert!(dimention.borrow() == "x".to_string() && *size == 3u32 && *offset == 3u32),
-            _ => panic!(),
+        if let Directive::SpacialMap {
+                        dimention,
+                        size,
+                        offset,
+                    } = DirectiveParser::new()
+                    .parse("SpatialMap (3,3)x;")
+                    .unwrap() {
+            assert!(dimention == "x".to_string() && size == 3u32 && offset == 3u32)
+        } else {
+            panic!()
         }
     }
 }
